0efe59551aef25d9ea75859a426e3727f8c3734f
philipslstwoyearse <braleosta@mail.ru>
2024-12-09 18:13:24 +0300

    newnewch

--- main.go
+++ main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"time"
-)
+import "fmt"
 
-//type HelloResponse struct {
-//	name string `json:"name"`
-//}
-
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		return
-	}
-	str := "name" + name
-	json.Marshal([]byte(str))
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(str))
-	w.WriteHeader(http.StatusOK)
-	log.Println("&s &S", time.Now().Format("2006/01/02 15:04:05"), fmt.Sprintf(`{"name":"%s"}`, name))
+func main() {
+	fmt.Println("Hello, Yandex Lyceum!")
 }
